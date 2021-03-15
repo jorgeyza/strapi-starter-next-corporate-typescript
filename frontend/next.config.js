f6d92502bfedd4f736940ec705e531175410c968
Jorge Eyzaguirre <jorgeyza@gmail.com>
2021-03-15 11:59:58 -0500

    update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   images: {
-    domains: [`${process.env.STRAPI_DOMAIN}`, 'localhost'],
+    domains: [`${process.env.STRAPI_DOMAIN}`],
+    deviceSizes: [
+      320,
+      480,
+      640,
+      750,
+      828,
+      960,
+      1080,
+      1200,
+      1440,
+      1920,
+      2048,
+      2560,
+      3440,
+      3840,
+    ],
   },
 };
